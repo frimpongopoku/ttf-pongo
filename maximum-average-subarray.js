e29a48ca4a7ceb1f1be2062da2e22c1a93275735
Frimpong <pongofrimi@gmail.com>
2023-05-25 18:03:33 +0400

    solved tahirus too

---
+++ maximum-average-subarray.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ * Runtime: O(n*k)
+ */
+var findMaxAverage = function(nums, k) {
+    let max =-10001;
+    let sum =0; 
+    let tracker = []
+    for (let i = 0; i < nums.length ; i++){
+        const num = nums[i]
+        sum+=num 
+        tracker.push(num)
+        if(tracker.length === k){
+            const average = sum/k
+            max = max  > average ? max : average 
+            const first = tracker[0]
+            tracker.splice(0,1)
+            sum-=first
+        }
+       
+    }
+
+    return max
+};
