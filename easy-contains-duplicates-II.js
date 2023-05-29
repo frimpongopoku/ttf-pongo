ddaea570d15055f4c3afce82118be16d0626098b
Frimpong <pongofrimi@gmail.com>
2023-05-29 18:57:30 +0400

    easy-contains-duplicates-II

---
+++ easy-contains-duplicates-II.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    const tracker = {} 
+    for(let i = 0; i < nums.length ; i++){
+        const value = nums[i]
+        const index = tracker[value]
+        if (index || index===0){
+            let diff = index - i 
+            diff = diff < 0 ? diff * -1 : diff 
+            if (diff <= k) return true
+        }
+        tracker[value] = i
+    }
+
+    return false
+    // O(n)
+};
